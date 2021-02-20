5b8e1510fb74751a09d8cacef90e2d9231b8c8c9
Liam Miller <millerliam@comcast.net>
2021-02-20 18:33:25 -0500

    Can pick up and drag block
    Trying to figure out how to code in click and drag for the blade. Got the block to move

--- Phonebooth Hockey/Assets/BladeBehavior.cs
+++ Phonebooth Hockey/Assets/BladeBehavior.cs
@@ -4,10 +4,20 @@ public class BladeBehavior : MonoBehaviour
 {
 
     public Transform player;
+
+    public Camera mainCamera;
+    private float cameraZDistance;
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Starting. Position: ");
+        Debug.Log(player.position.x);
+        Debug.Log(player.position.y);
+        // cameraZDistance = mainCamera.worldToScreenPoint(player.Position).z; // z axis of game object for screen view
     }
 
     // Update is called once per frame
@@ -16,10 +26,21 @@ public class BladeBehavior : MonoBehaviour
         //Debug.Log(player.position.z);
     }
 
+    void OnMouseDown() {
+        screenPoint = mainCamera.WorldToScreenPoint(player.position);
+        offset = player.position - mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+    }
+
     // when I click and drag in the y direction from the camera's point of view, I need it to move in the z direction on the ground
     void OnMouseDrag() {
         Debug.Log("we are dragging");
-        Debug.Log(Input.mousePosition.z);
+        Debug.Log(Input.mousePosition.y);
         Debug.Log(Input.mousePosition.x);
+
+        Vector3 currentScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 newWorldPosition = mainCamera.ScreenToWorldPoint(currentScreenPosition) + offset;
+        player.position = newWorldPosition;
+        //player.position.y = Input.mousePosition.y;
     }
 }
