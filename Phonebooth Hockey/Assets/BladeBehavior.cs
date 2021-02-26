47f186283396b94199877abfe537a3869e5c98a6
Liam Miller <millerliam@comcast.net>
2021-02-25 21:22:04 -0500

    Fixed issue with blade sinking in space
    Just had to turn off the gravity button in the inspector. probably was some overlap that was causing the issue

--- Phonebooth Hockey/Assets/BladeBehavior.cs
+++ Phonebooth Hockey/Assets/BladeBehavior.cs
@@ -7,10 +7,12 @@ public class BladeBehavior : MonoBehaviour
     public Transform blade;
     public Camera mainCamera;
 
-    private Vector3 screenPoint;
+    private Vector3 downVelcity = new Vector3(0.0f, -9.8f, 0.0f);
+
+    private Vector3 bladeScreenPoint;
     private Vector3 offset;
 
-    private Vector3 previousMousePosition;
+    private Vector3 previousBladeScreenPosition;
 
     private Vector3 mouseDragForce;
 
@@ -24,13 +26,19 @@ public class BladeBehavior : MonoBehaviour
 
     void FixedUpdate() {
 
+        Debug.Log(offset);
+
         // need to change mouseDragForce to normal force if user is not clicking and dragging
         if (Input.GetMouseButton(0)) {
-            Debug.Log("Fixed update and clicking down");
+            // Debug.Log("Fixed update and clicking down");
             bladeRigidBody.velocity = mouseDragForce * 0.1f;
+            mouseDragForce = new Vector3(0, 0, 0);
         } else {
-            Debug.Log("Fixed update and NOT CLICKING DOWN");
-            bladeRigidBody.velocity = mouseDragForce * 0f;
+            // Debug.Log("Fixed update and NOT CLICKING DOWN");
+            // bladeRigidBody.velocity = mouseDragForce * 0f;
+            bladeRigidBody.velocity = new Vector3(0.0f, -9.8f, 0.0f);
+            // bladeRigidBody.useGravity = false;
+            // bladeRigidBody.isKinematic = false;
         }
 
         // bladeRigidBody.velocity = mouseDragForce * 0.1f;
@@ -39,17 +47,24 @@ public class BladeBehavior : MonoBehaviour
     // Called when the user clicks down on the collider
     void OnMouseDown() {
 
-        screenPoint = mainCamera.WorldToScreenPoint(blade.position);
-        previousMousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        bladeScreenPoint = mainCamera.WorldToScreenPoint(blade.position);
+        offset = blade.position - mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, bladeScreenPoint.z));
+        previousBladeScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, bladeScreenPoint.z);
 
     }
 
     // when I click and drag in the y direction from the camera's point of view, I need it to move in the z direction on the ground
     void OnMouseDrag() {
 
-        Vector3 currentScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 currentBladeScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, bladeScreenPoint.z);
 
-        mouseDragForce = currentScreenPosition - previousMousePosition;
-        previousMousePosition = currentScreenPosition;
+        if (currentBladeScreenPosition.x > previousBladeScreenPosition.x) {
+            // Debug.Log("Going RIGHT");
+        } else if (currentBladeScreenPosition.x < previousBladeScreenPosition.x) {
+            // Debug.Log("Going LEFT");
+        }
+
+        mouseDragForce = currentBladeScreenPosition - previousBladeScreenPosition;
+        previousBladeScreenPosition = currentBladeScreenPosition;
     }
 }
